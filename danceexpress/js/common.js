e99c83bd6b698d0bd81e1014334ced3fc4ac4a32
developerFront <web-developer.front@yandex.ru>
2018-08-02 16:28:57 +0300

    123

--- danceexpress/js/common.js
+++ danceexpress/js/common.js
@@ -1,49 +1,61 @@
 $(document).ready(function() {
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-     items: 1,
-     loop: true,
-     margin: 10,
-     autoplay: true,
-     autoplayTimeout: 5000,
-     autoplayHoverPause: true
-  });
 
-  $(".student_item").on("click", function() {
+  $(".carousel .owl-carousel").owlCarousel({
+   items: 1,
+   loop: true,
+   margin: 10,
+   autoplay: true,
+   autoplayTimeout: 5000,
+   autoplayHoverPause: true
+ });
+
+  $(".carousel2 .owl-carousel").owlCarousel({
+   loop:true,
+   margin:10,
+   responsiveClass:true,
+   autoplay: true,
+   autoplayTimeout: 5000,
+   autoplayHoverPause: true,
+   responsive:{
+    0:{
+      items:2,
+      nav:true
+    },
+    600:{
+      items:3,
+      nav:true
+    },
+    1000:{
+      items:5,
+      nav:true,
+      
+    }
+  }
+});
+
+
+/*  $(".student_item").on("click", function() {
     if ($(this).is(".student_item_active")) {
       $(".student_item").removeClass("student_item_active");
     } else {
       $(".student_item").removeClass("student_item_active");
       $(this).addClass("student_item_active");
     }
+  });*/
+
+
+
+
+  $('.photo-popup-link').magnificPopup({type: 'image'}); // фото препода
+
+
+// Отзывы "начало"
+  $('.open-popup-link').magnificPopup({type:'inline'});
+
+  $(".student_item").each(function(i){
+    $(this).find("a").attr("href", "#work_" + i);
+    $(this).find(".student_item_content").attr("id", "work_" + i)
   });
+// Отзывы "конец""  
 
-  $(".student_item").hover(function() {
-    if ($(this).is(".student_item_active")) {
-      $(".student_item").removeClass("student_item_active");
-    } else {
-      $(".student_item").removeClass("student_item_active");
-      $(this).addClass("student_item_active");
-    }
-	});
-	
-	$(".reviews_student_text").on("click", function(){
-
-		if($(".reviews_student_block").css('display') == 'flex'){
-			$(".reviews_student_block").css('display', 'none');
-		}else{
-			$(".reviews_student_block").css('display', 'flex');
-
-		}
-	});
-
-	// function carouselService() {
-	// 	$('.item').each(function() {
-	// 		var t = $(this),
-	// 		h = t.find('.slide-text').outerHeight();
-	// 		t.find('.carousel-pic').css('min-height', h);
-	// 	});
-	// };
-	// carouselService();
-	
 });
